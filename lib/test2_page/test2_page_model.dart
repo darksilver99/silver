68001ba5452bef8e74d5ae54e0be658bd12a4307
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-10 06:37:08 +0000

    Updating to latest FlutterFlow output.

--- lib/test2_page/test2_page_model.dart
+++ lib/test2_page/test2_page_model.dart
@@ -3,7 +3,8 @@ import '/components/loading_view_widget.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
-import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -17,7 +18,8 @@ class Test2PageModel extends FlutterFlowModel {
 
   ///  State fields for stateful widgets in this page.
 
-  Completer<List<DataListRecord>>? firestoreRequestCompleter;
+  // Stores action output result for [Firestore Query - Query a collection] action in Test2Page widget.
+  List<DataListRecord>? dataListResult;
   // Model for loadingView component.
   late LoadingViewModel loadingViewModel;
 
@@ -34,19 +36,4 @@ class Test2PageModel extends FlutterFlowModel {
   /// Action blocks are added here.
 
   /// Additional helper methods are added here.
-
-  Future waitForFirestoreRequestCompleted({
-    double minWait = 0,
-    double maxWait = double.infinity,
-  }) async {
-    final stopwatch = Stopwatch()..start();
-    while (true) {
-      await Future.delayed(Duration(milliseconds: 50));
-      final timeElapsed = stopwatch.elapsedMilliseconds;
-      final requestComplete = firestoreRequestCompleter?.isCompleted ?? false;
-      if (timeElapsed > maxWait || (requestComplete && timeElapsed > minWait)) {
-        break;
-      }
-    }
-  }
 }
