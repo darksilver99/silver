dc8f488b9471b5308b0fb65560d26a50365c2d18
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-13 07:35:37 +0000

    Updating to latest FlutterFlow output.

--- lib/go_trip/go_trip_select_place_page/go_trip_select_place_page_model.dart
+++ lib/go_trip/go_trip_select_place_page/go_trip_select_place_page_model.dart
@@ -2,6 +2,7 @@ import '/flutter_flow/flutter_flow_autocomplete_options_list.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
--- lib/go_trip/go_trip_select_place_page/go_trip_select_place_page_widget.dart
+++ lib/go_trip/go_trip_select_place_page/go_trip_select_place_page_widget.dart
@@ -2,6 +2,7 @@ import '/flutter_flow/flutter_flow_autocomplete_options_list.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -132,6 +133,13 @@ class _GoTripSelectPlacePageWidgetState
                           controller: textEditingController,
                           focusNode: focusNode,
                           onEditingComplete: onEditingComplete,
+                          onChanged: (_) => EasyDebounce.debounce(
+                            '_model.textController',
+                            Duration(milliseconds: 2000),
+                            () async {
+                              setState(() {});
+                            },
+                          ),
                           obscureText: false,
                           decoration: InputDecoration(
                             labelText:
