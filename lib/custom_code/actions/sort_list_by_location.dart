2fa981d83a81cb22ef6d0fed1351822f01c237d7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-06 10:33:24 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/sort_list_by_location.dart
+++ lib/custom_code/actions/sort_list_by_location.dart
@@ -8,10 +8,47 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'dart:math';
+
 Future<List<dynamic>> sortListByLocation(
   LatLng? currentLoction,
   List<PlaceListRecord>? dataList,
 ) async {
   // Add your function code here!
-  return [{}];
+  print("sortListByLocation");
+  var newDataList = [];
+  for (var i = 0; i < dataList!.length; i++) {
+    var data = {
+      "title": dataList[i].title,
+      "image": dataList[i].image,
+      "distance": calculateDistance(dataList[i].location!, currentLoction!),
+    };
+    newDataList.add(data);
+  }
+  newDataList.sort((a, b) => a["distance"].compareTo(b["distance"]));
+  return newDataList;
+}
+
+double radians(double degrees) {
+  return degrees * (pi / 180);
+}
+
+double calculateDistance(LatLng start, LatLng end) {
+  const double radiusOfEarth = 6371; // Earth's radius in kilometers
+  final double lat1 = radians(start.latitude);
+  final double lon1 = radians(start.longitude);
+  final double lat2 = radians(end.latitude);
+  final double lon2 = radians(end.longitude);
+
+  final double dLat = lat2 - lat1;
+  final double dLon = lon2 - lon1;
+
+  final double a = sin(dLat / 2) * sin(dLat / 2) +
+      cos(lat1) * cos(lat2) * sin(dLon / 2) * sin(dLon / 2);
+  final double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+  final double distance = radiusOfEarth * c; // Distance in kilometers
+
+  //return distance;
+  return double.parse(distance.toStringAsFixed(2));
 }
