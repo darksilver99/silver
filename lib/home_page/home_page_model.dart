89ab2350b4b994c1c94027e57be1f4de2a2f0e4e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-18 10:24:14 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home_page_model.dart
+++ lib/home_page/home_page_model.dart
@@ -1,8 +1,11 @@
 import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'home_page_widget.dart' show HomePageWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -13,6 +16,8 @@ class HomePageModel extends FlutterFlowModel<HomePageWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  // Stores action output result for [Firestore Query - Query a collection] action in HomePage widget.
+  UsersRecord? aaaa;
 
   /// Initialization and disposal methods.
 
--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -1,7 +1,10 @@
 import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -49,6 +52,24 @@ class _HomePageWidgetState extends State<HomePageWidget> {
               },
             ) ??
             false;
+        if (confirmDialogResponse) {
+          _model.aaaa = await queryUsersRecordOnce(
+            singleRecord: true,
+          ).then((s) => s.firstOrNull);
+
+          context.pushNamed(
+            'CallingPage',
+            queryParameters: {
+              'userParameter': serializeParam(
+                _model.aaaa,
+                ParamType.Document,
+              ),
+            }.withoutNulls,
+            extra: <String, dynamic>{
+              'userParameter': _model.aaaa,
+            },
+          );
+        }
       }
     });
   }
