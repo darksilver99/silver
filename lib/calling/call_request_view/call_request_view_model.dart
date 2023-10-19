0ddff3aa51488245e5c54c5ace66022bbd87e303
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-19 07:49:12 +0000

    Updating to latest FlutterFlow output.

--- lib/calling/call_request_view/call_request_view_model.dart
+++ lib/calling/call_request_view/call_request_view_model.dart
@@ -1,10 +1,13 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'call_request_view_widget.dart' show CallRequestViewWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -13,6 +16,8 @@ class CallRequestViewModel extends FlutterFlowModel<CallRequestViewWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  // Stores action output result for [Backend Call - Read Document] action in CallRequestView widget.
+  UsersRecord? rsCaller;
 
   /// Initialization and disposal methods.
 
--- lib/calling/call_request_view/call_request_view_widget.dart
+++ lib/calling/call_request_view/call_request_view_widget.dart
@@ -1,9 +1,12 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -31,6 +34,12 @@ class _CallRequestViewWidgetState extends State<CallRequestViewWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => CallRequestViewModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      _model.rsCaller =
+          await UsersRecord.getDocumentOnce(widget.userParameter!.reference);
+    });
   }
 
   @override
