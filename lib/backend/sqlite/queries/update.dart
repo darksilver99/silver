ac87f4f2bc4d52279eaa57b6330d6bf9526e5bc8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-19 06:31:02 +0000

    Updating to latest FlutterFlow output.

--- lib/backend/sqlite/queries/update.dart
+++ lib/backend/sqlite/queries/update.dart
@@ -16,10 +16,11 @@ DELETE  FROM playlists WHERE  PlaylistId = ${id}
 /// BEGIN INSERTPLAYLIST
 Future performInsertPlaylist(
   Database database, {
+  int? id,
   String? name,
 }) {
   final query = '''
-INSERT INTO playlists  VALUES ('${name}')
+INSERT INTO playlists (PlaylistId, Name) VALUES (${id}, '${name}')
 ''';
   return database.rawQuery(query);
 }
